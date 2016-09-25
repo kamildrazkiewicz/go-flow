45caa0a798e56651ec65925484d738bf8c52410e
kamildrazkiewicz <drazkiewicz.k@gmail.com>
2016-09-25 16:56:20 +0200

    init commit

---
+++ example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"kamildrazkiewicz/go-flow"
+)
+
+func main() {
+	f1 := func(r *goflow.Results) (interface{}, error) {
+		fmt.Println("function1 started")
+		return 1, nil
+	}
+
+	f2 := func(r *goflow.Results) (interface{}, error) {
+		fmt.Println("function2 started", (*r)["f1"])
+		return "some results", nil
+	}
+
+	f3 := func(r *goflow.Results) (interface{}, error) {
+		fmt.Println("function3 started", (*r)["f1"])
+		return nil, nil
+	}
+
+	f4 := func(r *goflow.Results) (interface{}, error) {
+		fmt.Println("function4 started", r)
+		return nil, nil
+	}
+
+	res, err := goflow.New().
+		Add("f1", nil, f1).
+		Add("f2", []string{"f1"}, f2).
+		Add("f3", []string{"f1"}, f3).
+		Add("f4", []string{"f2", "f3"}, f4).
+		Do()
+
+	fmt.Println(res, err)
+}
---
+++ main.go
@@ -0,0 +1,97 @@
+package goflow
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Results map[string]interface{}
+
+type Flow interface {
+	New() *flow
+}
+
+type flow struct {
+	funcs map[string]*FlowStuct
+}
+
+type FlowStuct struct {
+	Deps    []string
+	Counter int
+	Fn      func(*Results) (interface{}, error)
+}
+
+func New() *flow {
+	a := &flow{}
+	a.funcs = make(map[string]*FlowStuct)
+	return a
+}
+
+func (a *flow) Add(name string, d []string, f func(res *Results) (interface{}, error)) *flow {
+	a.funcs[name] = &FlowStuct{
+		Deps:    d,
+		Fn:      f,
+		Counter: 1, // prevent deadlock
+	}
+	return a
+}
+
+func (a *flow) Do() (*Results, error) {
+	for fname, fn := range a.funcs {
+		for _, dep := range fn.Deps {
+			// prevent self depends
+			if dep == fname {
+				return nil, errors.New(fmt.Sprintf("Error: Function \"%s\" depends of it self!", fname))
+			}
+			// prevent no existing dependencies
+			if _, exists := a.funcs[dep]; exists == false {
+				return nil, errors.New(fmt.Sprintf("Error: Function \"%s\" not exists!", dep))
+			}
+			a.funcs[dep].Counter++
+		}
+	}
+	return a.do()
+}
+
+func (a *flow) do() (*Results, error) {
+	var lastErr error
+	res := make(Results, len(a.funcs))
+
+	// create flow channels
+	flow := make(map[string]chan interface{})
+	for name, _ := range a.funcs {
+		flow[name] = make(chan interface{})
+	}
+
+	for name, v := range a.funcs {
+		go func(name string, v *FlowStuct) {
+			results := make(Results, len(v.Deps))
+
+			// drain dependency results
+			for _, dep := range v.Deps {
+				results[dep] = <-flow[dep]
+			}
+
+			r, err := v.Fn(&results)
+			if err != nil {
+				// close all channels
+				for name, _ := range a.funcs {
+					close(flow[name])
+				}
+				lastErr = err
+				return
+			}
+			res[name] = r
+			for i := 0; i < v.Counter; i++ {
+				flow[name] <- r
+			}
+		}(name, v)
+	}
+
+	// wait for all
+	for name, _ := range a.funcs {
+		<-flow[name]
+	}
+
+	return &res, lastErr
+}
