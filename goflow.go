e99e5602c5dc5cd4b4eaa713d4a610a3a0c5716a
kamildrazkiewicz <drazkiewicz.k@gmail.com>
2017-01-02 10:43:01 +0100

    varible names refactor

--- goflow.go
+++ goflow.go
@@ -15,12 +15,20 @@ type flow struct {
 	funcs map[string]*flowStruct
 }
 
+type flowFunc func(res map[string]interface{}) (interface{}, error)
+
 type flowStruct struct {
 	Deps []string
 	Ctr  int
-	Fn   func(map[string]interface{}) (interface{}, error)
+	Fn   flowFunc
 	C    chan interface{}
 	once sync.Once
+}
+
+func (fs *flowStruct) Done(r interface{}) {
+	for i := 0; i < fs.Ctr; i++ {
+		fs.C <- r
+	}
 }
 
 func (fs *flowStruct) Close() {
@@ -29,7 +37,7 @@ func (fs *flowStruct) Close() {
 	})
 }
 
-func (fs *flowStruct) init() {
+func (fs *flowStruct) Init() {
 	fs.C = make(chan interface{}, fs.Ctr)
 }
 
@@ -40,71 +48,69 @@ func New() *flow {
 	}
 }
 
-func (f *flow) Add(name string, d []string, fn func(res map[string]interface{}) (interface{}, error)) *flow {
-	f.funcs[name] = &flowStruct{
+func (flw *flow) Add(name string, d []string, fn flowFunc) *flow {
+	flw.funcs[name] = &flowStruct{
 		Deps: d,
 		Fn:   fn,
 		Ctr:  1, // prevent deadlock
 	}
-	return f
+	return flw
 }
 
-func (f *flow) Do() (map[string]interface{}, error) {
-	for fname, fn := range f.funcs {
+func (flw *flow) Do() (map[string]interface{}, error) {
+	for name, fn := range flw.funcs {
 		for _, dep := range fn.Deps {
 			// prevent self depends
-			if dep == fname {
-
-				return nil, fmt.Errorf("Error: Function \"%s\" depends of it self!", fname)
+			if dep == name {
+				return nil, fmt.Errorf("Error: Function \"%s\" depends of it self!", name)
 			}
 			// prevent no existing dependencies
-			if _, exists := f.funcs[dep]; exists == false {
+			if _, exists := flw.funcs[dep]; exists == false {
 				return nil, fmt.Errorf("Error: Function \"%s\" not exists!", dep)
 			}
-			f.funcs[dep].Ctr++
+			flw.funcs[dep].Ctr++
 		}
 	}
-	return f.do()
+	return flw.do()
 }
 
-func (f *flow) do() (map[string]interface{}, error) {
-	var lastErr error
-	res := make(map[string]interface{}, len(f.funcs))
+func (flw *flow) do() (map[string]interface{}, error) {
+	var err error
+	res := make(map[string]interface{}, len(flw.funcs))
 
-	for name, v := range f.funcs {
-		v.init()
+	for name, f := range flw.funcs {
+		f.Init()
 		go func(name string, fs *flowStruct) {
 			defer func() { fs.Close() }()
 			results := make(map[string]interface{}, len(fs.Deps))
 
 			// drain dependency results
 			for _, dep := range fs.Deps {
-				results[dep] = <-f.funcs[dep].C
+				results[dep] = <-flw.funcs[dep].C
 			}
 
-			r, err := fs.Fn(results)
-			if err != nil {
+			r, fnErr := fs.Fn(results)
+			if fnErr != nil {
 				// close all channels
-				for _, v := range f.funcs {
-					v.Close()
+				for _, fn := range flw.funcs {
+					fn.Close()
 				}
-				lastErr = err
+				err = fnErr
 				return
 			}
 			// exit if error
-			if lastErr != nil {
+			if err != nil {
 				return
 			}
-			for i := 0; i < fs.Ctr; i++ {
-				fs.C <- r
-			}
-		}(name, v)
+			fs.Done(r)
+
+		}(name, f)
 	}
 
 	// wait for all
-	for name, fs := range f.funcs {
+	for name, fs := range flw.funcs {
 		res[name] = <-fs.C
 	}
 
-	return res, lastErr
+	return res, err
 }
